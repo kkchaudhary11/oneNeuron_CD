2b6b6fbb0572b6c21dd75bb21023432108a8d4a7
Kamal Kant <kkchaudhary11@gmail.com>
2021-09-24 17:33:32 +0530

    updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "kkc11"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author="kkc",
     author_email="kkchaudhary11@gmail.com",
     description="it is a implemenation of preceptron",
